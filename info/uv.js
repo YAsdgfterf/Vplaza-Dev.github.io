d5a7742ce368cb24c7e199e4988e99cfdbea9cd0
Orbit. <159073116+1056516@users.noreply.github.com>
2025-03-23 01:32:34 -0400

    Delete info directory

--- info/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/info/uv/uv.bundle.js');
-importScripts('/info/uv/uv.config.js');
-importScripts('/info/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
