0e9de6b86424d8f23bc7f88d9ef72ffa9838bf7d
Orbit. <159073116+1056516@users.noreply.github.com>
2025-03-23 01:23:23 -0400

    Update uv.js

--- info/uv.js
+++ info/uv.js
@@ -1,6 +1,6 @@
-importScripts('/active/uv/uv.bundle.js');
-importScripts('/active/uv/uv.config.js');
-importScripts('/active/uv/uv.sw.js');
+importScripts('/info/uv/uv.bundle.js');
+importScripts('/info/uv/uv.config.js');
+importScripts('/info/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
