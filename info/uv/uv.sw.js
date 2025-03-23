d42462318bba0b81c7521ebcf6253bca99f4bfbe
Orbit. <159073116+1056516@users.noreply.github.com>
2025-03-23 01:25:41 -0400

    Update uv.sw.js

--- info/uv/uv.sw.js
+++ info/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/active/uv/uv.bundle.js');
-importScripts('/active/uv/uv.config.js');
+importScripts('/info/uv/uv.bundle.js');
+importScripts('/info/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
